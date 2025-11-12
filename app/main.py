c1e9ee345b6c30da00feeb82fa358e7c4f6041ba
Sanjoli Sirohi <sanjoli.sirohi@gmail.com>
2025-11-11 18:12:36 -0600

    Respond with 200 #ia4

--- app/main.py
+++ app/main.py
@@ -8,9 +8,10 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
-    server_socket.recv(1024)
-    server_socket.sendall("HTTP/1.1 200 OK\r\n\r\n")
+    conn, addr = server_socket.accept() # wait for client
+    data = server_socket.recv(1024)
+    print(f"data: {data} \n")
+    conn.sendall("HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
