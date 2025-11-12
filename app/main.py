3dff8b2f9b8121253c0736c9590c055f74213b77
Sanjoli Sirohi <sanjoli.sirohi@gmail.com>
2025-11-11 18:33:28 -0600

    byte to string

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
-    data = conn.recv(1024)
+    data = conn.recv(1024).decode()
     print(f"data: {data} \n")
     conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
