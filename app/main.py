3f7ab02aca445e9212e49714567d7cfd381812f8
Sanjoli Sirohi <sanjoli.sirohi@gmail.com>
2025-11-11 18:44:47 -0600

    add http response

--- app/main.py
+++ app/main.py
@@ -1,5 +1,21 @@
 import socket  # noqa: F401
 
+def get_http_request()
+
+def get_http_response() -> bytes:
+    version: str = "HTTP/1.1"
+    status_code: int = 200
+    reason_phrase: int = "OK"
+
+    status_line: str = f"{version} {status_code} {reason_phrase}\r\n"
+
+    header: str = ""
+    headers: str = f"{header}\r\n"
+
+    response: str = f"{status_line}{headers}"
+
+    return response.encode()
+
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,7 +27,7 @@ def main():
     conn, addr = server_socket.accept() # wait for client
     data = conn.recv(1024).decode()
     print(f"data: {data} \n")
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    conn.sendall(get_http_response())
 
 
 if __name__ == "__main__":
